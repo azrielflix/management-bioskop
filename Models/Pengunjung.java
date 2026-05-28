3d2e602b6b868dac29cf0a8b66c52fafa189eefa
azrielflix <azrielflix21@gmail.com>
2026-05-28 15:39:26 +0800

    Menembahkan getter dan setter untuk class Pengunjung

--- Models/Pengunjung.java
+++ Models/Pengunjung.java
@@ -16,4 +16,53 @@ public class Pengunjung {
         this.jumlahTiket = jumlahTiket;
         this.totalHarga = totalHarga;
     }
+
+    // Getter dan Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getJudulFilm() {
+        return judulFilm;
+    }
+
+    public void setJudulFilm(String judulFilm) {
+        this.judulFilm = judulFilm;
+    }
+
+    public int getJumlahTiket() {
+        return jumlahTiket;
+    }
+
+    public void setJumlahTiket(int jumlahTiket) {
+        this.jumlahTiket = jumlahTiket;
+    }
+
+    public double getTotalHarga() {
+        return totalHarga;
+    }
+
+    public void setTotalHarga(double totalHarga) {
+        this.totalHarga = totalHarga;
+    }
 }
